d23fc77e8727b48fb1ae01c9b65c5285a407188a
mschubert <mschu.dev@gmail.com>
2017-06-11 00:20:25 +0100

    package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'symplex',
+      version = '0.1',
+      description = 'Add linear constrains via matrix equations',
+      url = 'https://github.com/mschubert/symplex',
+      author = 'Michael Schubert',
+      author_email = 'mschu.dev@gmail.com',
+      license = 'MIT',
+      packages = ['symplex'],
+      zip_safe = False)
